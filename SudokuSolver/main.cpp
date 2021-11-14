81d34e173ed38ac27738779ec4ca32355c0b575d
ManasviGaur <84803063+ManasviGaur@users.noreply.github.com>
2021-11-14 17:58:19 +0530

    Update main.cpp

--- SudokuSolver/main.cpp
+++ SudokuSolver/main.cpp
@@ -5,24 +5,19 @@ int main(){
     std::cout<<"Please Enter your choice\n ";
     std::cout<<"You can :-\n";
     std::cout<<"\t1.To Solve a Sudoku. (Enter 1)\n";
-
     std::cout<<"\t2.To Check the validity of an existing sudoku.(Enter 2)\n";
     std::cout<<"\t   --> ";
     std::cin>>choice;
-    if(choice==1)
-    {
+    if(choice==1){
         SudokuSolver s;
-    }
-    else if(choice==2)
-    {
+    }else if(choice==2){
         SudokuValidator s;
         if(s.checkValidity()){
             std::cout << "Your puzzle is valid!\n";
         }else{
             std::cout << "Your puzzle is invalid\n";
         }
-    }
-    else{
+    }else{
         while(true){
             std::cout<<"\nOops!! You have entered an invalid choice. Please Try again.\n";
             std::cout<<"\t   --> ";
